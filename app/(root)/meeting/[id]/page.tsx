d2e929f36b7058096f43da6955f04e74c95e40b9
zdawan <kumarjothi704@gmail.com>
2024-07-14 21:52:09 +0530

    Initial Commit

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,17 +1,26 @@
 "use client";
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
 
   const [isSetupComplete, setisSetupComplete] = useState(false);
+
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
   return;
-  <main className="h-screen w-full ">
-    <StreamCall>
+  <main className="h-screen w-full">
+    <StreamCall call={call}>
       <StreamTheme>
-        {!isSetupComplete ? "Meeting Setup" : "Meeting Room"}
+        {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
       </StreamTheme>
     </StreamCall>
   </main>;
---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MeetingSetup = () => {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-2xl font-bold">Setup Your Room</h1>
+    </div>
+  );
+};
+
+export default MeetingSetup;
